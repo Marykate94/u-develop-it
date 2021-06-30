3c08c025d6106735ede4c7f2e6d73326f6a6bf4d
Marykate Smith <Mkayy@Marykates-MacBook-Pro.local>
2021-06-29 20:37:30 -0400

    generates rows

--- server.js
+++ server.js
@@ -1,11 +1,26 @@
+const mysql = require('mysql2');
 const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
 
-
 // Express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+
+
+ // Connect to database
+ const db = mysql.createConnection(
+     {
+       host: 'localhost',
+       // Your MySQL username,
+       user: 'root',
+       // Your MySQL password
+       password: '#Brooklyn1950',
+       database: 'election'
+     },
+     console.log('Connected to the election database.')
+   );
 
 // used to verify connection with express
 // app.get('/', (req, res) => {
@@ -14,6 +29,9 @@ app.use(express.json());
 //     });
 // });
 
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+    console.log(rows);
+});
 // Default response for any other request (Not Found)
 app.use((req, res) => {
     res.status(404).end();
